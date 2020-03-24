ee142fcfa3725c81ee0d14b754aefdc4b4466db2
Markus Falk <mail@markus-falk.com>
2020-03-24 17:02:41 +0100

    feature: add rules package

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "rules": {
+    "type-enum" : [2, "always", [
+      // "feature",
+      "bugfix",
+      "hotfix",
+      "task"
+    ]],
+    "scope-enum": [2, "always", [
+      "test"
+    ]]
+  }
+}
